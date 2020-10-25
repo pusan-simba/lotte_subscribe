3c478860a1e82f28d6cd5e11899a9755e7770ebc
phaesol <parkhaesol0227@likelion.org>
2020-10-25 18:41:37 +0900

    item css 완성

--- lotte_subscribe/items/urls.py
+++ lotte_subscribe/items/urls.py
@@ -6,6 +6,7 @@ from .views import get_category,like_toggle, subscribe_toggle, search, get_mini_
 urlpatterns = [
     path('<int:category_id>/', get_category, name='category'),
     path('mini_category/<int:mini_category_id>',get_mini_category,name="get_mini_category"),
+   
     path('like/<int:item_id>/', like_toggle, name='like_toggle'),
     path('subscribe/<int:item_id>/', subscribe_toggle, name='subscribe_toggle'),
     path('search/', search, name='search'),
